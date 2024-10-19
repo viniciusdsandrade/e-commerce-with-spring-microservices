bce3370499105c18ea398ff4f2517c97b39e83d3
Andrade <vinicius_andrade2010@hotmail.com>
2024-10-19 15:44:57 -0300

    fix: mongoDB customer connection

--- service/customer/src/main/java/org/restful/customer/customer/dto/CustomerRequest.java
+++ service/customer/src/main/java/org/restful/customer/customer/dto/CustomerRequest.java
@@ -31,14 +31,19 @@ import org.restful.customer.customer.entity.Address;
  */
 @Schema(description = "DTO para requisições de criação e atualização de clientes.")
 public record CustomerRequest(
+
         String id,
+
         @NotNull(message = "First name is required")
         String firstName,
+
         @NotNull(message = "Last name is required")
         String lastName,
+
         @NotNull(message = "Email is required")
         @Email(message = "Email is invalid")
         String email,
+
         Address address
 ) {
 }
--- service/product/src/main/java/org/restful/product/ProductApplication.java
+++ service/product/src/main/java/org/restful/product/ProductApplication.java
@@ -1,13 +1,12 @@
 package org.restful.product;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class ProductApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        run(ProductApplication.class, args);
+    }
 }
